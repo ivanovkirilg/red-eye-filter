eb068fac6732956a795202304a9d472f88fbd63d
ivanovkirilg <ivanovkirilg@gmail.com>
2022-09-02 17:36:13 +0300

    Find red pixels

--- src/solution/Solution.cpp
+++ src/solution/Solution.cpp
@@ -1,15 +1,44 @@
 #include "Solution.hpp"
 
+#include <algorithm>
+#include <cstdint>
+#include <set>
+
 #include "utils/FunctionTracer.hpp"
 
-void Solution::compute(std::vector<PackedImage> &images)
+void Solution::compute([[maybe_unused]]std::vector<PackedImage> &images)
 {
   FunctionTracer<std::chrono::milliseconds> tracer("compute", "ms");
-  //TODO: fill solution
 }
 
 void Solution::compute(std::vector<StrideImage> &images)
 {
   FunctionTracer<std::chrono::milliseconds> tracer("compute", "ms");
-  //TODO: fill solution
+
+  using PixelIterator = std::vector<uint8_t>::iterator;
+
+  for (auto & img : images)
+  {
+    std::set<PixelIterator> checkedRedPixels;
+
+    PixelIterator redPixel = img.redPixels.begin();
+    const PixelIterator end = img.redPixels.end();
+
+    auto next = [&]()
+    {
+      redPixel = std::find_if(redPixel, end,
+        [](uint8_t redValue) { return redValue >= 200; });
+
+      return redPixel;
+    };
+
+    while (next() != end)
+    {
+      std::cout << (int) *redPixel << ' ';
+
+      redPixel++;
+    }
+
+    std::cout << '\n';
+  }
 }
