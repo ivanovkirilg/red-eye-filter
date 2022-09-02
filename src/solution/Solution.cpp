97c60c24b9dc813d136262aa77f8ca578c02afd6
ivanovkirilg <ivanovkirilg@gmail.com>
2022-09-02 22:55:54 +0300

    Clean up a bit
    Braces
    Remove needless caching
    Lambda (next red pixel)

--- src/solution/Solution.cpp
+++ src/solution/Solution.cpp
@@ -8,76 +8,52 @@
 #include "utils/EyePatterns.hpp"
 #include "utils/FunctionTracer.hpp"
 
-namespace
-{
-  bool isPrettyRed(uint8_t pixelRedValue)
-  {
-    return pixelRedValue >= 200;
-  }
+namespace {
+
+bool isPrettyRed(uint8_t pixelRedValue) {
+  return pixelRedValue >= 200;
 }
 
-void Solution::compute(std::vector<StrideImage> &images)
-{
+} // Anonymous namespace
+
+void Solution::compute(std::vector<StrideImage> &images) {
   FunctionTracer<std::chrono::milliseconds> tracer("compute", "ms");
 
   using PixelIterator = std::vector<uint8_t>::iterator;
 
-  for (auto & img : images)
-  {
-    std::set<PixelIterator> checkedRedPixels;
-
+  for (auto & img : images) {
     PixelIterator redPixel = img.redPixels.begin();
     const PixelIterator end = img.redPixels.end();
 
-    auto next = [&]()
-    {
+    auto next = [end](PixelIterator& redPixel) {
       redPixel = std::find_if(redPixel + 1, end, isPrettyRed);
-
       return redPixel;
     };
 
-    while (next() != end)
-    {
-      // std::cout << (int) *redPixel << ' ';
-
+    while (next(redPixel) != end) {
       // Look for pattern
-      if (checkedRedPixels.find(redPixel) == checkedRedPixels.end())
-      {
-        for (auto pattern = EYE_PATTERNS.rbegin(); pattern < EYE_PATTERNS.rend(); pattern++)
-        {
-          bool match = true;
+      for (auto pattern = EYE_PATTERNS.rbegin(); pattern < EYE_PATTERNS.rend(); pattern++) {
+        bool match = true;
 
-          for (int32_t row = 0; match && (row < EYE_PATTERN_COL_SIZE); row++)
-          {
-            for (int32_t col = 0; match && (col < EYE_PATTERN_COL_SIZE); col++)
-            {
-              if (!isspace((*pattern)[row][col]) &&
-                  !isPrettyRed(redPixel[(img.resolution.width * row) + col]))
-              {
-                match = false;
-              }
+        for (int32_t row = 0; match && (row < EYE_PATTERN_COL_SIZE); row++) {
+          for (int32_t col = 0; match && (col < EYE_PATTERN_COL_SIZE); col++) {
+            if (!isspace((*pattern)[row][col]) &&
+                !isPrettyRed(redPixel[(img.resolution.width * row) + col])) {
+              match = false;
             }
           }
+        }
 
-          if (match)
-          {
-            for (int32_t row = 0; match && (row < EYE_PATTERN_COL_SIZE); row++)
-            {
-              for (int32_t col = 0; match && (col < EYE_PATTERN_COL_SIZE); col++)
-              {
-                if (!isspace((*pattern)[row][col]))
-                {
-                  redPixel[(img.resolution.width * row) + col] -= 150;
-                }
+        if (match) {
+          for (int32_t row = 0; match && (row < EYE_PATTERN_COL_SIZE); row++) {
+            for (int32_t col = 0; match && (col < EYE_PATTERN_COL_SIZE); col++) {
+              if (!isspace((*pattern)[row][col])) {
+                redPixel[(img.resolution.width * row) + col] -= 150;
               }
             }
-
-            checkedRedPixels.insert(redPixel);
           }
         }
       }
     }
-
-    // std::cout << '\n';
   }
 }
