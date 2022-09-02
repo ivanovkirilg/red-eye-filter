878c64079f1e7aefe46974c806121a2908046d33
ivanovkirilg <ivanovkirilg@gmail.com>
2022-09-02 19:42:50 +0300

    A working mess

--- src/solution/Solution.cpp
+++ src/solution/Solution.cpp
@@ -1,10 +1,20 @@
 #include "Solution.hpp"
 
 #include <algorithm>
+#include <cctype>
 #include <cstdint>
 #include <set>
 
+#include "utils/EyePatterns.hpp"
 #include "utils/FunctionTracer.hpp"
+
+namespace
+{
+  bool isPrettyRed(uint8_t pixelRedValue)
+  {
+    return pixelRedValue >= 200;
+  }
+}
 
 void Solution::compute([[maybe_unused]]std::vector<PackedImage> &images)
 {
@@ -26,19 +36,53 @@ void Solution::compute(std::vector<StrideImage> &images)
 
     auto next = [&]()
     {
-      redPixel = std::find_if(redPixel, end,
-        [](uint8_t redValue) { return redValue >= 200; });
+      redPixel = std::find_if(redPixel + 1, end, isPrettyRed);
 
       return redPixel;
     };
 
     while (next() != end)
     {
-      std::cout << (int) *redPixel << ' ';
+      // std::cout << (int) *redPixel << ' ';
 
-      redPixel++;
+      // Look for pattern
+      if (checkedRedPixels.find(redPixel) == checkedRedPixels.end())
+      {
+        for (auto pattern = EYE_PATTERNS.rbegin(); pattern < EYE_PATTERNS.rend(); pattern++)
+        {
+          bool match = true;
+
+          for (int32_t row = 0; match && (row < EYE_PATTERN_COL_SIZE); row++)
+          {
+            for (int32_t col = 0; match && (col < EYE_PATTERN_COL_SIZE); col++)
+            {
+              if (!isspace((*pattern)[row][col]) &&
+                  !isPrettyRed(redPixel[(img.resolution.width * row) + col]))
+              {
+                match = false;
+              }
+            }
+          }
+
+          if (match)
+          {
+            for (int32_t row = 0; match && (row < EYE_PATTERN_COL_SIZE); row++)
+            {
+              for (int32_t col = 0; match && (col < EYE_PATTERN_COL_SIZE); col++)
+              {
+                if (!isspace((*pattern)[row][col]))
+                {
+                  redPixel[(img.resolution.width * row) + col] -= 150;
+                }
+              }
+            }
+
+            checkedRedPixels.insert(redPixel);
+          }
+        }
+      }
     }
 
-    std::cout << '\n';
+    // std::cout << '\n';
   }
 }
